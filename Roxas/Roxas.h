2fec0a7f4f8e51412f82299cacbd8431b4a19622
Riley Testut <riley@rileytestut.com>
2014-08-27 15:48:56 -0500

    Initial Commit

---
+++ Roxas/Roxas.h
@@ -0,0 +1,19 @@
+//
+//  Roxas.h
+//  Roxas
+//
+//  Created by Riley Testut on 8/27/14.
+//  Copyright (c) 2014 Riley Testut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Roxas.
+FOUNDATION_EXPORT double RoxasVersionNumber;
+
+//! Project version string for Roxas.
+FOUNDATION_EXPORT const unsigned char RoxasVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Roxas/PublicHeader.h>
+
+
