40484dd040f42ac58efe93deae770a1cbd1213e5
Riley Testut <riley@rileytestut.com>
2014-11-05 02:31:06 -0600

    Added RSTNavigationController
    RSTNavigationController is a subclass of UINavigationController that
    forwards all rotation methods to the topViewController

---
+++ Roxas/RSTNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  RSTNavigationController.h
+//  Roxas
+//
+//  Created by Riley Testut on 11/5/14.
+//  Copyright (c) 2014 Riley Testut. All rights reserved.
+//
+
+@import UIKit;
+
+@interface RSTNavigationController : UINavigationController
+
+@end
---
+++ Roxas/RSTNavigationController.m
@@ -0,0 +1,46 @@
+//
+//  RSTNavigationController.m
+//  Roxas
+//
+//  Created by Riley Testut on 11/5/14.
+//  Copyright (c) 2014 Riley Testut. All rights reserved.
+//
+
+#import "RSTNavigationController.h"
+
+@interface RSTNavigationController ()
+
+@end
+
+@implementation RSTNavigationController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Rotation -
+
+- (BOOL)shouldAutorotate
+{
+    return [self.topViewController shouldAutorotate];
+}
+
+- (NSUInteger)supportedInterfaceOrientations
+{
+    return [self.topViewController supportedInterfaceOrientations];
+}
+
+- (UIInterfaceOrientation)preferredInterfaceOrientationForPresentation
+{
+    return [self.topViewController preferredInterfaceOrientationForPresentation];
+}
+
+@end
