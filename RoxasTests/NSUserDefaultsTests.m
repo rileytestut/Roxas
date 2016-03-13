a4430d69f12b7ded3930f7bc7ed78d896693307d
Riley Testut <riley@rileytestut.com>
2016-03-13 17:20:13 -0500

    Adds more NSUserDefault+DynamicProperties tests
    • Non-Dynamic Properties
    • Custom NSUserDefault subclasses

--- RoxasTests/NSUserDefaultsTests.m
+++ RoxasTests/NSUserDefaultsTests.m
@@ -27,6 +27,11 @@
 // Invalid
 @property (assign, nonatomic) short shortProperty;
 
+// Custom Accessors
+@property (copy, nonatomic, getter=getMyCustomProperty, setter=setMyCustomProperty:) NSString *customAccessorsProperty;
+
+@property (copy, nonatomic) NSString *nonDynamicProperty;
+
 @end
 
 @implementation NSUserDefaults (TestProperties)
@@ -38,8 +43,34 @@
 @dynamic URLProperty;
 @dynamic objectProperty;
 @dynamic shortProperty;
+@dynamic customAccessorsProperty;
+
+- (void)setNonDynamicProperty:(NSString *)nonDynamicProperty
+{
+}
+
+- (NSString *)nonDynamicProperty
+{
+    return nil;
+}
 
 @end
+
+
+
+@interface RSTUserDefaults : NSUserDefaults
+
+// Non-Dynamic Property
+@property (copy, nonatomic) NSString *subclassProperty;
+
+@end
+
+@implementation RSTUserDefaults
+
+@synthesize subclassProperty;
+
+@end
+
 
 
 
@@ -57,6 +88,14 @@
     
     NSString *bundleIdentifier = [[NSBundle bundleForClass:self.class] bundleIdentifier];
     self.userDefaults = [[NSUserDefaults alloc] initWithSuiteName:bundleIdentifier];
+}
+
+- (void)tearDown
+{
+    [super tearDown];
+    
+    NSString *bundleIdentifier = [[NSBundle bundleForClass:self.class] bundleIdentifier];
+    [[NSUserDefaults standardUserDefaults] removePersistentDomainForName:bundleIdentifier];
 }
 
 - (void)testBOOLs
@@ -141,6 +180,9 @@
     self.userDefaults.objectProperty = @{@"Roxas": @1};
     XCTAssertEqualObjects(@{@"Roxas": @1}, self.userDefaults.objectProperty);
     
+    // Unfortunately, trying to test this just results in the test deadlocking :/
+    // XCTAssertThrows(self.userDefaults.objectProperty = @{@"operation": [NSOperation new]});
+    
     self.userDefaults.objectProperty = [@"Roxas" dataUsingEncoding:NSUTF8StringEncoding];
     XCTAssertEqualObjects([@"Roxas" dataUsingEncoding:NSUTF8StringEncoding], self.userDefaults.objectProperty);
     
@@ -152,6 +194,31 @@
 {
     XCTAssertThrows(self.userDefaults.shortProperty);
     XCTAssertThrows(self.userDefaults.shortProperty = 1);
+}
+
+- (void)testPersistence
+{
+    self.userDefaults.customAccessorsProperty = @"Roxas";
+    XCTAssertEqualObjects(@"Roxas", self.userDefaults.customAccessorsProperty);
+    XCTAssertEqualObjects(@"Roxas", self.userDefaults.dictionaryRepresentation[@"customAccessorsProperty"]);
+    
+    self.userDefaults.nonDynamicProperty = @"Sora";
+    XCTAssertNil(self.userDefaults.nonDynamicProperty);
+    XCTAssertNil(self.userDefaults.dictionaryRepresentation[@"nonDynamicProperty"]);
+}
+
+- (void)testCustomSubclass
+{
+    NSString *bundleIdentifier = [[NSBundle bundleForClass:self.class] bundleIdentifier];
+    RSTUserDefaults *customUserDefaults = [[RSTUserDefaults alloc] initWithSuiteName:bundleIdentifier];
+    
+    customUserDefaults.subclassProperty = @"Sora";
+    XCTAssertEqualObjects(@"Sora", customUserDefaults.subclassProperty);
+    XCTAssertNil(customUserDefaults.dictionaryRepresentation[@"subclassProperty"]);
+    
+    customUserDefaults.objectProperty = @"Riley";
+    XCTAssertEqualObjects(@"Riley", customUserDefaults.objectProperty);
+    XCTAssertEqualObjects(@"Riley", customUserDefaults.dictionaryRepresentation[@"objectProperty"]);
 }
 
 @end
