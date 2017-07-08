c974c356c5523967642b18dc285a9b5a03bf512a
Riley Testut <riley@rileytestut.com>
2017-07-07 21:09:27 -0500

    Fixes misc. UICollectionView+CellContent issues
    • Fixes issue where performBatchUpdates:’ completionBlock is not called if batch updates don’t result in an animation
    • Runs RSTCellContentChangeOperation on main thread
    • Handles nested beginUpdates/endUpdates correctly

--- Roxas/UICollectionView+CellContent.m
+++ Roxas/UICollectionView+CellContent.m
@@ -15,7 +15,8 @@
 
 @interface UICollectionView ()
 
-@property (nullable, nonatomic, strong) NSOperationQueue *rst_operationQueue;
+@property (nonatomic) NSInteger rst_nestedUpdatesCounter;
+@property (nullable, nonatomic) NSMutableArray<RSTCellContentChangeOperation *> *rst_operations;
 
 @end
 
@@ -23,20 +24,38 @@
 
 - (void)beginUpdates
 {
-    self.rst_operationQueue = [[NSOperationQueue alloc] init];
-    self.rst_operationQueue.maxConcurrentOperationCount = 1;
-    self.rst_operationQueue.suspended = YES;
+    if (self.rst_nestedUpdatesCounter == 0)
+    {
+        self.rst_operations = [NSMutableArray array];
+    }
+    
+    self.rst_nestedUpdatesCounter++;
 }
 
 - (void)endUpdates
 {
+    if (self.rst_nestedUpdatesCounter <= 0)
+    {
+        return;
+    }
+    
+    self.rst_nestedUpdatesCounter--;
+    
+    if (self.rst_nestedUpdatesCounter > 0)
+    {
+        return;
+    }
+    
+    NSArray<RSTCellContentChangeOperation *> *operations = [self.rst_operations copy];
+    self.rst_operations = nil;
+    
     // According to documentation:
     // Move is reported when an object changes in a manner that affects its position in the results.  An update of the object is assumed in this case, no separate update message is sent to the delegate.
     
     // Therefore, we need to manually send another update message to items that moved after move is complete
     // (because it may crash if you try to update an item that is moving in the same batch updates block...)
-    NSMutableArray<RSTCellContentChangeOperation *> *postMoveUpdateOperations = [NSMutableArray array];
-    for (RSTCellContentChangeOperation *operation in self.rst_operationQueue.operations)
+    __block NSMutableArray<RSTCellContentChangeOperation *> *postMoveUpdateOperations = [NSMutableArray array];
+    for (RSTCellContentChangeOperation *operation in operations)
     {
         if (operation.change.type != RSTCellContentChangeMove)
         {
@@ -49,24 +68,37 @@
         [postMoveUpdateOperations addObject:updateOperation];
     }
     
-    [self performBatchUpdates:^{
-        [self.rst_operationQueue setSuspended:NO];
-        [self.rst_operationQueue waitUntilAllOperationsAreFinished];
-    } completion:^(BOOL finished) {
+    [CATransaction begin];
+    [CATransaction setCompletionBlock:^{
         
-        // Perform additional updates after any moved items have been moved
+        // Perform additional updates after any moved items have been moved.
+        // These additional updates must be performed after the first batch of operations have finished animating, or else the animation looks weird.
+        // However, the completion block for performBatchUpdates: is only called if the updates result in an animation.
+        // Since there is no way to know if an animation will actually occur (dependent on multiple factors), we explicitly create our own CATransaction.
+        // If there are no animations, the CATransaction's completion block will be called immediately. If there *are* animations, the completion block will be called after the animations finish.
+                
         [self performBatchUpdates:^{
-            [self.rst_operationQueue addOperations:postMoveUpdateOperations waitUntilFinished:YES];
-        } completion:^(BOOL finished) {
-            self.rst_operationQueue = nil;
-        }];
+            for (RSTCellContentChangeOperation *operation in postMoveUpdateOperations)
+            {
+                [operation start];
+            }
+        } completion:nil];
     }];
+    
+    [self performBatchUpdates:^{
+        for (RSTCellContentChangeOperation *operation in operations)
+        {
+            [operation start];
+        }
+    } completion:nil];
+    
+    [CATransaction commit];
 }
 
 - (void)addChange:(RSTCellContentChange *)change
 {
     RSTCollectionViewChangeOperation *operation = [[RSTCollectionViewChangeOperation alloc] initWithChange:change collectionView:self];
-    [self.rst_operationQueue addOperation:operation];
+    [self.rst_operations addObject:operation];
 }
 
 #pragma mark - Getters/Setters -
@@ -76,14 +108,25 @@
     return @protocol(UICollectionViewDataSource);
 }
 
-- (NSOperationQueue *)rst_operationQueue
+- (NSInteger)rst_nestedUpdatesCounter
 {
-    return objc_getAssociatedObject(self, @selector(rst_operationQueue));
+    return [objc_getAssociatedObject(self, @selector(rst_nestedUpdatesCounter)) integerValue];
 }
 
-- (void)setRst_operationQueue:(NSOperationQueue *)rst_operationQueue
+- (void)setRst_nestedUpdatesCounter:(NSInteger)rst_nestedUpdatesCounter
 {
-    objc_setAssociatedObject(self, @selector(rst_operationQueue), rst_operationQueue, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+    NSNumber *value = (rst_nestedUpdatesCounter != 0) ? @(rst_nestedUpdatesCounter) : nil;
+    objc_setAssociatedObject(self, @selector(rst_nestedUpdatesCounter), value, OBJC_ASSOCIATION_COPY_NONATOMIC);
+}
+
+- (NSMutableArray<RSTCellContentChangeOperation *> *)rst_operations
+{
+    return objc_getAssociatedObject(self, @selector(rst_operations));
+}
+
+- (void)setRst_operations:(NSMutableArray<RSTCellContentChangeOperation *> *)rst_operations
+{
+    objc_setAssociatedObject(self, @selector(rst_operations), rst_operations, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
 }
 
 @end
